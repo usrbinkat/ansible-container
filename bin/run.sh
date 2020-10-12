b3114e5b4a8bc3af4995bdb01da38a7b2b7abba3
dlystra <dlystra@redhat.com>
2020-10-12 12:14:31 -0500

    added oc login command

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,7 @@
 PLAYBOOK_DIR=$(echo ${GIT_URL} | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')
 GIT_URL=$(echo ${GIT_URL} | cut -f3- -d '/')
 
+oc login --token=${OC_TOKEN} --server=${OC_API_URL} --insecure-skip-tls-verify
 git clone https://${GIT_USER}:${GIT_TOKEN}@${GIT_URL}
 cd ${PLAYBOOK_DIR}
 ansible-playbook site.yml
